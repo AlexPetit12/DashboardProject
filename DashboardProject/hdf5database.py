3161d625b2130bbf9622e783f5246b54f1e3444b
alexpetit <alexpetit12@hotmail.com>
2018-06-05 22:41:43 -0400

    Added functions to hdf5database.

--- DashboardProject/hdf5database.py
+++ DashboardProject/hdf5database.py
@@ -1,6 +1,23 @@
-import h5py
+import pandas as pd
 
-from
+from yahoofinance import get_symbol_data
 
-with h5py.File('/home/alexpetit/Databases/mytestfile.hdf5', 'w') as f:
-    dset = f.create_dataset('mydataset', (100,), dtype='i')
+symbol = 'SPY'
+df = get_symbol_data(symbol)
+
+path = '/home/alexpetit/Databases/ts_storage.h5'
+group = symbol
+
+
+def write_df_to_hdfstorage(df, path_to_storage, group):
+    hdf = pd.HDFStore(path_to_storage)
+    hdf.put(group, df)
+    hdf.close()
+
+
+def read_df_from_hdfstorage(path_to_storage, group):
+    return pd.read_hdf(path_to_storage, group)
+
+
+write_df_to_hdfstorage(df, path, group)
+df = read_df_from_hdfstorage(path, group)
