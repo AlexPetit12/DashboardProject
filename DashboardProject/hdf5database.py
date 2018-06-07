c9f86d42a5d9ca77ee7066d95209cb23f5c4adb7
alexpetit <alexpetit12@hotmail.com>
2018-06-06 22:26:10 -0400

    Removed example.

--- DashboardProject/hdf5database.py
+++ DashboardProject/hdf5database.py
@@ -1,13 +1,5 @@
 import pandas as pd
 
-from yahoofinance import get_symbol_data
-
-symbol = 'SPY'
-df = get_symbol_data(symbol)
-
-path = '/home/alexpetit/Databases/ts_storage.h5'
-group = symbol
-
 
 def write_df_to_hdfstorage(df, path_to_storage, group):
     hdf = pd.HDFStore(path_to_storage)
@@ -17,7 +9,3 @@ def write_df_to_hdfstorage(df, path_to_storage, group):
 
 def read_df_from_hdfstorage(path_to_storage, group):
     return pd.read_hdf(path_to_storage, group)
-
-
-write_df_to_hdfstorage(df, path, group)
-df = read_df_from_hdfstorage(path, group)
